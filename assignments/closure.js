9981481a0fccd50e647905c8c9c5ab448f0c5375
Miikis <28997062+Miikis@users.noreply.github.com>
2019-05-07 12:17:42 -0700

    Add Task 4 + Stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -6,14 +6,38 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
     // Return a function that when invoked increments and returns a counter variable.
+    let count = 0
+    return () => ++count
 }
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter()
+console.log(newCounter())
+console.log(newCounter())
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
     // Return an object that has two methods called `increment` and `decrement`.
     // `increment` should increment a counter variable in closure scope and return it.
     // `decrement` should decrement the counter variable and return it.
+    let count = 0
+
+    return {
+        increment: () => {
+            ++count
+            return count
+        },
+        decrement: () => {
+            --count
+            return count
+        }
+    }
 }
+const countObject = counterFactory()
+console.log(countObject.increment())
+console.log(countObject.decrement())
+console.log(countObject.increment())
+console.log(countObject.decrement())
+console.log(countObject.increment())
+console.log(countObject.increment())
