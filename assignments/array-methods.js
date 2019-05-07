211f3772f962d7759db9bbac88d4d8e8fed9755a
Miikis <28997062+Miikis@users.noreply.github.com>
2019-05-07 12:08:19 -0700

    Finish Task 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -458,28 +458,51 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = []
+runners.forEach(runner =>
+    fullName.push(`${runner.first_name} ${runner.last_name}`)
+)
 console.log(fullName)
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = []
+let allCaps = runners.map(runner => {
+    const newRunner = Object.assign(runner)
+    newRunner.first_name = runner.first_name.toUpperCase()
+    return newRunner
+})
 console.log(allCaps)
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = []
+let largeShirts = runners.filter(runner => runner.shirt_size === "L")
 console.log(largeShirts)
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = []
+let ticketPriceTotal = runners.reduce(
+    (acc, current) => acc + current.donation,
+    0
+)
 console.log(ticketPriceTotal)
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+const emailList = runners.map(runner => runner.email)
+console.log(emailList)
 
 // Problem 2
+const domains = emailList.map(email => email.split("@")[1])
+console.log(domains)
 
 // Problem 3
+const uniqueDomains = Array.from(
+    domains.reduce((acc, current) => acc.add(current), new Set())
+)
+console.log(
+    uniqueDomains,
+    `${runners.length} domains total => ${
+        uniqueDomains.length
+    } of them are unique`
+)
